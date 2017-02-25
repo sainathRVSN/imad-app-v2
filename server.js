fc14d024b0c628070cda9b2ede62ebbfd0d55201
sainathRVSN <sainadh.yama@gmail.com>
2017-02-25 21:54:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send('article-one requested is served');
+});
+app.get('/article-two',function(req,res){
+    res.send('article-two requested is served');
+});
+app.get('/article-three',function(req,res){
+    res.send('article-three requested is served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
