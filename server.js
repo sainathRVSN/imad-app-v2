9800e0456fc5cc968b42b6f0878d6d6369ac4b3f
sainathRVSN <sainadh.yama@gmail.com>
2017-02-26 21:10:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.get('/:articlename',function(req,res){
   var articlename=req.params.articlename;
   res.send(createtemplate(articles[articlename]));
 });
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
