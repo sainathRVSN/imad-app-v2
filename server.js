1ecc0dd7a4dc835455c7bb249008344515bd3982
sainathRVSN <sainadh.yama@gmail.com>
2017-02-26 17:53:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,19 +4,68 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles={
+ 'article-one':{
+    title:'first article sainath',
+    heading:'article one',
+    date:'26 mar 2017',
+    content:`<p>create a project create a project create a project create a project create a projectcreate a projectcreate a projectcreate a projectcreate a projectcreate a project create a projectcreate a project create a project create a project</p>`
+},
+  'article-two':{
+    title:'second article sainath',
+    heading:'article two',
+    date:'26 mar 2017',
+    content:`<p>create a project create a project create a project create a project create a projectcreate a projectcreate a projectcreate a projectcreate a projectcreate a project create a projectcreate a project create a project create a project</p>`
+},
 
+'article-three':{
+    title:'third article sainath',
+    heading:'article three',
+    date:'26 mar 2017',
+    content:`<p>create a project create a project create a project create a project create a projectcreate a projectcreate a projectcreate a projectcreate a projectcreate a project create a projectcreate a project create a project create a project</p>`
+}
+};
+
+function createtemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmltemplate=
+`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name='viewport' content="'width-device-width',initial-scale=1"/>
+       <link rel='stylesheet' href='ui/style.css'> 
+    </head>
+    <body>
+        <div class="container">
+        <div>
+        <a href='/'>home</a>
+        <hr>
+        <h3>${heading}</h3>
+        <div>
+        ${date}
+        </div>
+        ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return(htmltemplate);
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articlename',function(req,res){
+  var articlename=req.params.articlename;
+  res.send(createtemplate(articles[articlename]));
 });
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
