018b1156b64f1d54667f20e840e253d434d9dd8c
sainathRVSN <sainadh.yama@gmail.com>
2017-02-27 14:29:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML="new value";
+//var element=document.getElementById('main-text');
+//element.innerHTML="new value";
 //var img=document.getElementById('madi');
 /*img.onclick=function(){
     img.style.marginLeft ='100px';
