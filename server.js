e0db850f19ef1a276d3bdbbc89104d8a47a2abe0
sainathRVSN <sainadh.yama@gmail.com>
2017-03-08 19:05:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,16 +78,17 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-app.get('/:articleName', function (req,res){
-var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName]));
-});
 var names=[];
 app.get('/submit-name',function(req,res){//   /submit-name?name=xxxx
     var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
+app.get('/:articleName', function (req,res){
+var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
+});
+
 app.get('/ui/style.css', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
