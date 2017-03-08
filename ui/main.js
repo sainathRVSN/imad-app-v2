af207e3c888b50a62c1a1c88bec56e99a3141e32
sainathRVSN <sainadh.yama@gmail.com>
2017-03-08 16:41:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,20 @@
-console.log('Loaded!');
-/*var element=document.getElementById('main-text');
-element.innerHTML="new value";*/
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+//counter code
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //create a Http request
+  var request=new XmlHttpRequest();  
+  //capture the response and store in some variable
+  request.onreadystate=function(){
+      if(request.readyState==XmlHttpRequest.DONE){
+          //take some action
+          if(request.status==200){
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+      }
+      }
+  };
+  //Make a request
+  request.open('GET','http://sainadhrvsn.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
