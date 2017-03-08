f8d6fe829e76f2c7f128d362d6f9adeb7ececbfe
sainathRVSN <sainadh.yama@gmail.com>
2017-03-08 19:32:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,17 +8,33 @@ button.onclick=function(){
       if(request.readyState ===  XMLHttpRequest.DONE){
           //take some action
           if(request.status  ===  200){
-          //submit name
-    var nameInput=document.getElementById('name');
-   
-  submit.onclick=function(){
-    //Make a request to the server and send the name
-    
-    //capture the name and render it to a list
-    var name=nameInput.value;
-    var submit=document.getElementById('submit_btn');
-    var names=request.responseText;
-    names=JSON.parse(names);
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+      }
+      }
+  };
+  //Make a request
+  request.open('GET','http://sainathrvsn.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
+//submit name
+
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+      //create a request object
+  var request=new XMLHttpRequest();  
+  //capture the response and store in some variable
+  request.onreadystatechange=function(){
+      if(request.readyState ===  XMLHttpRequest.DONE){
+          //take some action
+          if(request.status  ===  200){
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+      }
+      }
+    var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i < names.length;i++)
     {
@@ -29,10 +45,8 @@ button.onclick=function(){
     
     
 };
-      }
-      }
-  };
-  //Make a request
-  request.open('GET','http://sainathrvsn.imad.hasura-app.io/submit-name?name='+ name,true);
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+request.open('GET','http://sainathrvsn.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
 };
